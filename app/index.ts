e5fdeb5b9419a85ca25cc8d88c86a152edaed60a
tm312 <tmoellers@protonmail.ch>
2022-10-05 16:36:22 +0800

    feat: Add initial app main script

---
+++ app/index.ts
@@ -0,0 +1,47 @@
+import * as core from '@actions/core';
+import Setup from './lib/setup';
+import File from './lib/file';
+import sendRequest from './lib/sendRequest';
+
+async function main() {
+  try {
+    Setup.debug()
+    Setup.checkRequiredVarsAvailable('GITHUB_TOKEN')
+
+    // inputs from action
+    const filepath: string = core.getInput('filepath');
+    const url: string = core.getInput('url');
+
+    // Check if file path exist and file content can be loaded 
+    const file = new File(filepath);
+    
+    // send request to API
+    const response = await sendRequest(url, file.filepath);
+
+    // const statusCode = response.status;
+    // const data = response.data;
+    // const outputObject = {
+    //   url,
+    //   method,
+    //   statusCode,
+    //   data
+    // };
+
+    // const consoleOutputJSON = JSON.stringify(outputObject, undefined, 2);
+    // console.log(consoleOutputJSON);
+
+    // if (statusCode >= 400) {
+    //   core.setFailed(`HTTP request failed with status code: ${statusCode}`);
+    // } else {
+    //   const outputJSON = JSON.stringify(outputObject);
+    //   core.setOutput('output', outputJSON);
+    // }
+  } catch (error) {
+    console.log(error);
+    core.setOutput('Set filepath', core.getInput('filepath'));
+    core.setOutput('Set url', core.getInput('url'));
+    core.setFailed(error.message);
+  }
+}
+
+main();
