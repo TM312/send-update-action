4d80b3eaf144bb5343802a51d206f72b6fe144a0
tm312 <tmoellers@protonmail.ch>
2022-10-07 16:23:30 +0800

    fix: Get file content & filepath

--- src/lib/file.ts
+++ src/lib/file.ts
@@ -11,17 +11,17 @@ class File {
     let filepathFull = null;
     let fileContent = null;
 
-    // if (!!filepath) {
-    //   if (fs.statSync(root).isDirectory()) {
-    //     filepathFull = path.join(root, filepath)
-    //   }
+    if (!!filepath) {
+      if (fs.statSync(root).isDirectory()) {
+        filepathFull = path.join(root, filepath)
+      }
 
-    //   if (!fs.existsSync(filepathFull)) {
-    //     throw new Error(`The file ${filepathFull} does not exist found.`)
-    //   }
+      if (!filepathFull || (!!filepathFull && !fs.existsSync(filepathFull))) {
+        throw new Error(`The file ${filepathFull} does not exist found.`)
+      }
 
-    //   fileContent = JSON.parse(fs.readFileSync(filepathFull, { encoding: 'utf-8' }))
-    // }
+      fileContent = JSON.parse(fs.readFileSync(filepathFull, { encoding: 'utf-8' }))
+    }
 
     this.filepath = filepathFull;
     this.fileContent = fileContent;
