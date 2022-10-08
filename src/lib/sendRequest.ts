08d1eb14437f5b12f5f83c6e346bc35eafa9841d
tm312 <tmoellers@protonmail.ch>
2022-10-08 08:55:05 +0800

    nit: Rm ws

--- src/lib/sendRequest.ts
+++ src/lib/sendRequest.ts
@@ -1,10 +1,8 @@
 import axios from 'axios';
 
-
 async function sendFile(url: string, data: any) {
-    // const options = { headers: '', maxContentLength: Infinity }
+    //// const options = { headers: '', maxContentLength: Infinity }
     return axios.post(url, data)
 }
-
 
 export default sendFile;
